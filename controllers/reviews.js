edff3ca4c053db06d93015db40b6c3b8aceca706
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-23 15:14:34 -0400

    reviews controllers done

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,9 +4,50 @@ const reviewsApi = require('../models/reviews.js')
 
 const reviewsRouter = express.Router()
 
+
 reviewsRouter.get('/', (req, res) => {
-    res.send(templateApi.getHelloWorldString())
-  })
+    reviewsApi.getAllReviews()
+        .then(reviews => {
+            res.render('/reviews/allReviews', { reviews })
+        })
+})
+
+reviewsRouter.get('/new', (req, res) => {
+    res.render('reviews/newReview')
+})
+
+reviewsRouter.get('/:reviewId/editReview', (req, res) => {
+    reviewsApi.getOneReview(req.params.reviewId)
+        .then(review => {
+            res.render('reviews/editReview', { review })
+        })
+})
+
+reviewsRouter.get('/:reviewId', (req, res) => {
+    reviewsApi.getOneReview(req.params.reviewId)
+        .then(location => {
+            res.render('reviews/review', { location })
+        })
+})
+
+reviewsRouter.post('/', (req, res) => {
+    reviewsApi.addNewReview(req.body)
+        .then(() => {
+            res.redirect('/reviews')
+        })
+})
+
+reviewsRouter.put('/:reviewsId', (req, res) => {
+    reviewsApi.updateReview(req.params.reviewId, req.body)
+        .then(() => {
+            res.redirect('/reviews')
+        })
+})
+
+reviewsRouter.delete('/:reviewId', (req, res) => {
+    reviewsApi.deleteReview(req.params.reviewId)
+        .then(res.redirect('/reviews'))
+})
 
   module.exports = {
     reviewsRouter
