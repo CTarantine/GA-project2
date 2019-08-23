097b3d7a64337f73ab2703ab14ebd9cb61d1133d
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-23 09:50:53 -0400

    location schema complete

--- models/location.js
+++ models/location.js
@@ -13,4 +13,26 @@ const getAllLocations = () => {
     return locationCollection.find()
 }
 
-const get
+const getLocation = (locationId) => {
+    return locationCollection.findById(locationId)
+}
+
+const addNewLocation = (newLocation) => {
+    return locationCollection.insertMany(newLocation)
+}
+
+const updateLocation = (locationId,updatedLocation) => {
+    return locationCollection.findByIdAndUpdate(locationId,updatedLocation)
+}
+
+const deleteLocation = (id) =>{
+    return locationCollection.findByIdAndDelete({_id:id})
+}
+
+module.exports = {
+    getAllLocations,
+    getLocation,
+    addNewLocation,
+    updateLocation,
+    deleteLocation 
+}
