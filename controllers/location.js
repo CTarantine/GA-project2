9b73f030981b062aca9fdafff97be0745fbab81e
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-23 12:19:20 -0400

    location controllers in progress

--- controllers/location.js
+++ controllers/location.js
@@ -4,10 +4,43 @@ const locationApi = require('../models/location.js')
 
 const locationRouter = express.Router()
 
-locationRouter.get('/', (req, res) => {
-    res.send(templateApi.getHelloWorldString())
-  })
 
-  module.exports = {
+locationRouter.get('/', (req, res) => {
+    locationApi.getAllLocations()
+        .then(locations => {
+            res.render('/location/allLocations', { locations })
+        })
+})
+
+locationRouter.get('/new', function (req, res) {
+    res.render('location/newLocation')
+})
+
+locationRouter.get('/:locationId/editLocation', (req, res) => {
+    res.render('createShop', {})
+})
+
+shopRouter.get('/:index', function (req, res) {
+    const singleShop = shopsApi.getShop(req.params.index)
+    res.render('editShop', { singleShop, index: req.params.index })
+})
+
+//CHANGE .SEND() TO .REDIRECT(PAGE)
+shopRouter.post('/', (req, res) => {
+    shopsApi.addShop(req.body);
+    res.send(200);
+})
+
+shopRouter.put('/:index', function (req, res) {
+    shopsApi.updateShop(req.params.index, req.body);
+    res.send(200);
+})
+
+shopRouter.delete('/:index', function (req, res) {
+    shopsApi.deleteShop(req.params.index)
+    res.send(200);
+})
+
+module.exports = {
     locationRouter
-  }
+}
