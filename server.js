64ab39d0638b1f070faad84a83688f333c78c8aa
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-23 19:37:50 -0400

    server modification

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const hbs = require('hbs')
 
 
 const { homePageRouter } = require('./controllers/home.js')
